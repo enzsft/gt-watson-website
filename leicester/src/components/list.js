cfd7580bf7c0f7494033f7422153c5a97b7933b3
leecheneler <lacheneler@gmail.com>
2020-01-24 23:32:37 +0000

    updated list

--- leicester/src/components/list.js
+++ leicester/src/components/list.js
@@ -1,18 +1,21 @@
 import styled from "styled-components"
 
-import { spacers } from "../theme"
+import { spacers, typography } from "../theme"
 
 export const List = styled.ul`
   margin: 0;
   padding: 0;
   list-style: none;
-  margin-top: -${spacers.lg};
+  margin-top: -${spacers.md};
 `
 
 export const ListItem = styled.li`
   padding-top: ${spacers.md};
 `
 
-export const ItemTitle = styled.h3`
+export const ItemTitle = styled.span`
+  display: block;
   padding-bottom: ${spacers.sm};
+  font-weight: 600;
+  font-size: ${typography.fontSizes.sm};
 `
